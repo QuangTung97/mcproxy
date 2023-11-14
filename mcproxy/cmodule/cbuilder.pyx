9b50c10039bb663153cfbf5e15974b1deed84ab4
tung.tq <tung.tq@teko.vn>
2023-11-14 17:03:24 +0700

    Add Builder MGet

--- mcproxy/cmodule/cbuilder.pyx
+++ mcproxy/cmodule/cbuilder.pyx
@@ -1,5 +1,7 @@
 from libc.string cimport memcpy
 
+import cython
+
 from cutil cimport alloc_object, free_object
 
 
@@ -12,6 +14,7 @@ ctypedef int (*write_func)(void *obj, const char *data, int n) noexcept
 cdef struct Builder:
     char buf[2 * MAX_DATA]
     int buf_len
+    int write_limit
 
     void *write_obj
     write_func write_fn
@@ -23,10 +26,11 @@ cdef struct MGetCmd:
     int N
 
 
-cdef Builder *make_builder(void *write_obj, write_func write_fn) noexcept nogil:
+cdef Builder *make_builder(void *write_obj, write_func write_fn, int limit) noexcept nogil:
     cdef Builder *b = <Builder *>alloc_object(sizeof(Builder))
 
     b.buf_len = 0
+    b.write_limit = limit
 
     b.write_obj = write_obj
     b.write_fn = write_fn
@@ -44,24 +48,63 @@ cdef void builder_append(Builder *b, const char *data, int n) noexcept nogil:
     b.buf_len += n
 
 
+@cython.cdivision
+cdef void builder_append_num(Builder *b, int num) noexcept nogil:
+    cdef char buf[16]
+    cdef int num_len = 0
+    cdef char zero = '0'
+    cdef char ch
+    cdef int i
+
+    if num <= 0:
+        builder_append(b, '0', 1)
+        return
+
+    while num > 0:
+        ch = (num % 10) + zero
+        buf[num_len] = ch
+        num_len += 1
+
+        num = num // 10
+    
+    for i in range(num_len // 2):
+        buf[i], buf[num_len - 1 - i] = buf[num_len - 1 - i], buf[i]
+    
+    builder_append(b, buf, num_len)
+
+
 cdef int builder_add_mget(Builder *b, MGetCmd cmd) noexcept nogil:
+    cdef int ret
+
     builder_append(b, 'mg ', 3)
     builder_append(b, cmd.key, cmd.key_len)
+
+    if cmd.N > 0:
+        builder_append(b, ' N', 2)
+        builder_append_num(b, cmd.N)
+
     builder_append(b, ' v\r\n', 4)
+
+    if b.buf_len > b.write_limit:
+        ret = builder_flush(b, b.write_limit)
+        if ret:
+            return ret
+        memcpy(b.buf, b.buf + b.write_limit, b.buf_len)
+
     return 0
 
 
-cdef int builder_flush(Builder *b) noexcept:
+cdef int builder_flush(Builder *b, int n) noexcept nogil:
     cdef int ret
-    ret = b.write_fn(b.write_obj, b.buf, b.buf_len)
-    b.buf_len = 0
+    with gil:
+        ret = b.write_fn(b.write_obj, b.buf, n)
+        b.buf_len -= n
     return ret
 
 
 cdef int builder_finish(Builder *b) noexcept nogil:
     if b.buf_len > 0:
-        with gil:
-            return builder_flush(b)
+        return builder_flush(b, b.buf_len)
     return 0
 
 
@@ -78,18 +121,18 @@ cdef class BuilderTest:
     cdef Builder *b
     cdef object write_obj
 
-    def __cinit__(self, object write_fn):
+    def __cinit__(self, object write_fn, int limit):
         self.write_obj = write_fn
-        self.b = make_builder(<void *>write_fn, python_write_func)
+        self.b = make_builder(<void *>write_fn, python_write_func, limit)
 
     def __dealloc__(self):
         free_builder(self.b)
     
-    def add_mget(self, bytes key):
+    def add_mget(self, bytes key, int N = 0):
         cdef const char *ptr = key
         cdef int key_len = len(key)
 
-        cdef MGetCmd cmd = MGetCmd(key=ptr, key_len=key_len, N=0)
+        cdef MGetCmd cmd = MGetCmd(key=ptr, key_len=key_len, N=N)
         return builder_add_mget(self.b, cmd)
     
     def finish(self):
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -16,12 +16,12 @@ class TestBuilder(unittest.TestCase):
         return 0
 
     def test_empty(self) -> None:
-        b = cbuilder.BuilderTest(self.write_func)
+        b = cbuilder.BuilderTest(self.write_func, 1024)
         del b
         self.assertEqual(0, cutil.py_get_mem())
 
     def test_add_mget(self) -> None:
-        b = cbuilder.BuilderTest(self.write_func)
+        b = cbuilder.BuilderTest(self.write_func, 1024)
 
         ret = b.add_mget(b'key01')
         self.assertEqual(0, ret)
@@ -37,7 +37,7 @@ class TestBuilder(unittest.TestCase):
         self.assertEqual(0, cutil.py_get_mem())
 
     def test_add_mget_multi(self) -> None:
-        b = cbuilder.BuilderTest(self.write_func)
+        b = cbuilder.BuilderTest(self.write_func, 1024)
 
         ret = b.add_mget(b'key01')
         self.assertEqual(0, ret)
@@ -63,6 +63,82 @@ class TestBuilder(unittest.TestCase):
             b'mg key01 v\r\nmg key02 v\r\n',
             b'mg key03 v\r\n',
         ], self.write_list)
+
+        del b
+        self.assertEqual(0, cutil.py_get_mem())
+
+    def test_add_mget_exceed_limit(self) -> None:
+        cmd1 = b'mg key01 v\r\n'
+
+        b = cbuilder.BuilderTest(self.write_func, len(cmd1))
+
+        self.assertEqual(0, b.add_mget(b'key01'))
+        self.assertEqual([], self.write_list)
+
+        self.assertEqual(0, b.add_mget(b'k2'))
+        self.assertEqual([b'mg key01 v\r\n'], self.write_list)
+
+        self.assertEqual(0, b.finish())
+        self.assertEqual([b'mg key01 v\r\n', b'mg k2 v\r\n'], self.write_list)
+
+        self.assertEqual(0, b.add_mget(b'key02'))
+        self.assertEqual(0, b.add_mget(b'key03'))
+
+        self.assertEqual(0, b.finish())
+        self.assertEqual([
+            b'mg key01 v\r\n', b'mg k2 v\r\n',
+            b'mg key02 v\r\n', b'mg key03 v\r\n',
+        ], self.write_list)
+
+        del b
+        self.assertEqual(0, cutil.py_get_mem())
+
+    def test_add_mget_near_exceed_limit_on_first_call(self) -> None:
+        cmd1 = b'mg key01 v\r\n'
+
+        b = cbuilder.BuilderTest(self.write_func, len(cmd1) + 1)
+
+        self.assertEqual(0, b.add_mget(b'key01'))
+        self.assertEqual([], self.write_list)
+
+        self.assertEqual(0, b.add_mget(b'k2'))
+        self.assertEqual([b'mg key01 v\r\nm'], self.write_list)
+
+        self.assertEqual(0, b.finish())
+        self.assertEqual([b'mg key01 v\r\nm', b'g k2 v\r\n'], self.write_list)
+
+        del b
+        self.assertEqual(0, cutil.py_get_mem())
+
+    def test_add_mget_with_N(self) -> None:
+        b = cbuilder.BuilderTest(self.write_func, 1024)
+
+        self.assertEqual(0, b.add_mget(b'key01', N=12))
+        self.assertEqual(0, b.finish())
+
+        self.assertEqual([b'mg key01 N12 v\r\n'], self.write_list)
+
+        del b
+        self.assertEqual(0, cutil.py_get_mem())
+
+    def test_add_mget_with_N_zero(self) -> None:
+        b = cbuilder.BuilderTest(self.write_func, 1024)
+
+        self.assertEqual(0, b.add_mget(b'key01', N=0))
+        self.assertEqual(0, b.finish())
+
+        self.assertEqual([b'mg key01 v\r\n'], self.write_list)
+
+        del b
+        self.assertEqual(0, cutil.py_get_mem())
+
+    def test_add_mget_with_N_negative(self) -> None:
+        b = cbuilder.BuilderTest(self.write_func, 1024)
+
+        self.assertEqual(0, b.add_mget(b'key01', N=-2))
+        self.assertEqual(0, b.finish())
+
+        self.assertEqual([b'mg key01 v\r\n'], self.write_list)
 
         del b
         self.assertEqual(0, cutil.py_get_mem())
